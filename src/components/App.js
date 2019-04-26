cff7a1230b8ade28f0346af859cd24a933370b93
Unknown <f.thery3103@gmail.com>
2019-04-26 16:15:05 +0200

    Routing

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,6 @@ const App = ({children}) => {
             {children}
         </Fragment>
 
-
     )
 }
 
---
+++ src/components/Root.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router,Redirect,Route,Switch } from 'react-router-dom'
+import Index from "./Index";
+import App from "./App";
+
+
+
+const Root = ({ store }) =>  (
+    <Provider store={store}>
+        <Router>
+            <App>
+                <Route exact path="/" component={Index}/>
+            </App>
+        </Router>
+    </Provider>
+)
+
+
+
+Root.propTypes = {
+    store: PropTypes.object.isRequired
+}
+
+export default Root
