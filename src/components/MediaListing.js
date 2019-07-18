59edb607fc4f0473bacbb57d88b3ba6559e99c53
Barogthor <f.thery3103@gmail.com>
2019-07-18 23:52:36 +0200

    medialist -> card

--- src/components/MediaListing.js
+++ src/components/MediaListing.js
@@ -3,9 +3,11 @@ import {Topbar} from "./Topbar";
 import {connect} from "react-redux";
 import {sign_in} from "../actions/userActions";
 import {Link, Redirect} from "react-router-dom";
-import {Button, Cell, Colors} from "react-foundation";
+import {Button, Cell, Colors, Grid} from "react-foundation";
 import {read_themes} from "../actions/themeActions";
 import {delete_media, read_medias} from "../actions/mediaAction";
+import {faTrash, faImage, faMusic, faFilm} from "@fortawesome/free-solid-svg-icons/index";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome/index.es";
 
 class MediaListing extends Component{
 
@@ -19,27 +21,48 @@ class MediaListing extends Component{
     }
 
     deleteMedia(event){
+        event.preventDefault()
         const mediaId = event.target.getAttribute("media")
         console.log("delete",event.target.getAttribute("media"));
         this.props.dispatch(delete_media(this.props.theme.id, mediaId, this.props.auth.token))
     }
 
     renderOnline(){
-        console.log("MEdia list",this.props);
+        console.log("Media list",this.props);
         return (
             <Fragment>
 
                 <h1>Medias</h1>
-                <ul>
+                <Grid gutters={"padding"}>
                     {
                         Object.values(this.props.medias).map(media => {
-                            return (<li key={media.id}>
-                                <Link to={`/theme/${media.theme_id}/media/${media.id}`}><Button color={Colors.SUCCESS}>{media.title}</Button></Link>
-                                <Button color={Colors.ALERT} onClick={this.deleteMedia} media={media.id}>Delete</Button>
-                            </li>)
+                            return (
+                                <Cell small={6} medium={4} large={3} key={media.id}>
+                                    <div className="card mycard">
+                                        <Link to={`/theme/${media.theme_id}/media/${media.id}`}>
+                                            <div className="card-divider">
+                                                {media.title}
+                                                <Button className={"bt-card-delete"} color={Colors.ALERT} onClick={this.deleteMedia} media={media.id}><FontAwesomeIcon icon={faTrash} /></Button>
+                                            </div>
+                                            <div className="card-section txtCenter" style={{fontSize: "3rem"}}>
+                                                {
+                                                    media.kind === 'picture'
+                                                    ? <FontAwesomeIcon icon={faImage} />
+                                                    : media.kind === 'music'
+                                                    ? <FontAwesomeIcon icon={faMusic} />
+                                                    : media.kind === 'movie' || media.kind === 'video'
+                                                    ? <FontAwesomeIcon icon={faFilm} />
+                                                    : ''
+                                                }
+                                            </div>
+                                        </Link>
+                                    </div>
+                                </Cell>
+
+                            )
                         })
                     }
-                </ul>
+                </Grid>
             </Fragment>
         )
     }
