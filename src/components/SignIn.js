e9984e4d1cd9aa3f41688be90b07864857fa2241
Barogthor <f.thery3103@gmail.com>
2019-07-19 17:15:52 +0200

    Sign styles

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -3,9 +3,10 @@ import {Topbar} from "./Topbar";
 import {connect} from "react-redux";
 import {sign_in} from "../actions/userActions";
 import {Redirect} from "react-router-dom";
+import {Button, Cell, Grid} from "react-foundation";
 
-class SignIn extends Component{
-    constructor(props){
+class SignIn extends Component {
+    constructor(props) {
         super(props)
         this.logIn = this.logIn.bind(this)
         this.updateEmail = this.updateEmail.bind(this)
@@ -13,52 +14,53 @@ class SignIn extends Component{
         this.state = {username: "", password: ""}
     }
 
-    componentDidMount(){
+    componentDidMount() {
     }
 
-    updatePassword(event){
+    updatePassword(event) {
         this.setState({password: event.target.value})
     }
 
-    updateEmail(event){
+    updateEmail(event) {
         this.setState({username: event.target.value})
     }
 
-    logIn(event){
+    logIn(event) {
         event.preventDefault()
-        const payload = {email:this.state.username, password:this.state.password}
+        const payload = {email: this.state.username, password: this.state.password}
         this.props.dispatch(sign_in(JSON.stringify(payload)))
     }
 
-    render(){
+    render() {
         const {auth} = this.props
         // if(auth.token && auth.token.length>0) window.location.href = "/home"
         return !auth.token ?
             <Fragment>
 
-                <form method={"post"} onSubmit={this.logIn} >
-                        <div className={"grid-x grid-padding-x"}>
-                            <div className={"medium-6 small-12 cell"}>
-                                <label>Email
-                                    <input type={"text"} name={"email"} value={this.state.username} onChange={this.updateEmail} />
-                                </label>
-                            </div>
-                            <div className={"medium-6 small-12 cell"}>
-                                <label>Password
-                                    <input type="password" name={"password"} value={this.state.password} onChange={this.updatePassword} />
-                                </label>
-                            </div>
-                            <div className={"medium-6 small-12 cell"}>
-                                <button className={"button"} type={"submit"} >Sign In</button>
-                            </div>
-                        </div>
+                <form method={"post"} onSubmit={this.logIn}>
+                    <Grid gutters={"padding"}>
+                        <Cell className={"input-field"} small={12} medium={6}>
+                            <input type={"text"} name={"email"} value={this.state.username} required
+                                   onChange={this.updateEmail}/>
+                            <label>Email</label>
+
+                        </Cell>
+                        <Cell className={"input-field"} small={12} medium={6}>
+                            <input type="password" name={"password"} value={this.state.password} required
+                                   onChange={this.updatePassword}/>
+                            <label>Password</label>
+                        </Cell>
+                        <Cell small={12} medium={12}>
+                            <Button type={"submit"}>Sign In</Button>
+                        </Cell>
+                    </Grid>
                 </form>
             </Fragment>
-        : <Redirect to={"/"} />
+            : <Redirect to={"/"}/>
     }
 }
 
-function mapStateToProps(state, ownProps){
+function mapStateToProps(state, ownProps) {
     console.log("mapStateToProps: SignIn");
     return {
         auth: state.app.auth,
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,69 +2,90 @@ import React, {Component, Fragment} from 'react'
 import {Topbar} from "./Topbar";
 import {connect} from "react-redux";
 import {sign_in, sign_up} from "../actions/userActions";
+import {Button, Cell, Grid} from "react-foundation";
 
-class SignUp extends Component{
-    constructor(props){
+class SignUp extends Component {
+    constructor(props) {
         super(props)
         this.signUp = this.signUp.bind(this)
+        this.updateEmail = this.updateEmail.bind(this)
+        this.updatePassword = this.updatePassword.bind(this)
+        this.updatePasswordConfirm = this.updatePasswordConfirm.bind(this)
+        this.updateUsername = this.updateUsername.bind(this)
+        this.state = {username: "", password: "", password_confirmation: "", email: ""}
     }
 
-    componentDidMount(){
-        console.log("SignUp mounted");
+    componentDidMount() {
     }
 
-    signUp(event){
+    signUp(event) {
         event.preventDefault()
-        console.log(event.target)
-        const form = event.target
-        const username = form.querySelector('[name="username"]').value
-        const password = form.querySelector('[name="password"]').value
-        const password_confirmation = form.querySelector('[name="password_confirmation"]').value
-        const email = form.querySelector('[name="email"]').value
-        const payload = {email:email, nickname:username, password:password, password_confirmation:password_confirmation}
+        const payload = this.state
         console.table(payload)
         this.props.dispatch(sign_up(JSON.stringify(payload)))
     }
 
-    render(){
+    updateUsername(event) {
+        this.setState({username: event.target.value})
+    }
+
+    updatePassword(event) {
+        this.setState({password: event.target.value})
+    }
+
+    updatePasswordConfirm(event) {
+        this.setState({password_confirmation: event.target.value})
+    }
+
+    updateEmail(event) {
+        this.setState({email: event.target.value})
+    }
+
+    render() {
         return (
             <Fragment>
 
-                <form method={"post"} onSubmit={this.signUp} >
-                        <div className={"grid-x grid-padding-x"}>
-                            <div className={"medium-6 small-12 cell"}>
-                                <label>Username
-                                    <input type={"text"} name={"username"} />
-                                </label>
-                            </div>
-                            <div className={"medium-6 small-12 cell"}>
-                                <label>Email
-                                    <input type={"text"} name={"email"} />
-                                </label>
-                            </div>
-                            <div className={"medium-6 small-12 cell"}>
-                                <label>Password
-                                    <input type="password" name={"password"}/>
-                                </label>
-                            </div>
-                            <div className={"medium-6 small-12 cell"}>
-                                <label>Password Confirmation
-                                    <input type="password" name={"password_confirmation"}/>
-                                </label>
-                            </div>
-                            <div className={"medium-6 small-12 cell"}>
-                                <button className={"button"} type={"submit"} >Sign Up</button>
-                            </div>
-                        </div>
+                <form method={"post"} onSubmit={this.signUp}>
+                    <Grid gutters={"padding"}>
+                        <Cell className={"input-field"} small={12} medium={6}>
+                            <input type={"text"} name={"username"} onChange={this.updateUsername} required
+                                   value={this.state.username}/>
+                            <label>Username</label>
+
+                        </Cell>
+                        <Cell className={"input-field"} small={12} medium={6}>
+                            <input type={"text"} name={"email"} onChange={this.updateEmail} value={this.state.email}
+                                   required/>
+                            <label>Email</label>
+                        </Cell>
+                        <Cell className={"input-field"} small={12} medium={6}>
+                            <input type="password" name={"password"} onChange={this.updatePassword} required
+                                   value={this.state.password}/>
+                            <label>Password</label>
+                        </Cell>
+                        <Cell className={"input-field"} small={12} medium={6}>
+                            <input type="password" name={"password_confirmation"} onChange={this.updatePasswordConfirm}
+                                   required
+                                   value={this.state.password_confirmation}/>
+                            <label>Password Confirmation</label>
+                        </Cell>
+                        <Cell small={12}>
+                        </Cell>
+
+                        <Cell small={12}>
+                            <a href={"/privacy-policy"} target="_blank">See our policy</a><br/>
+                            <Button type={"submit"} style={{marginTop: "0.5rem"}}>Sign Up</Button>
+                        </Cell>
+                    </Grid>
                 </form>
             </Fragment>
         )
     }
 }
 
-function mapStateToProps(state, ownProps){
+function mapStateToProps(state, ownProps) {
     console.log("mapStateToProps: SignUp");
-     return {}
+    return {}
 }
 
 export default connect(mapStateToProps)(SignUp)
